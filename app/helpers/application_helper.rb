75516d558208273039854f9163cb95563a10b047
bashforger <bash.forger@gmail.com>
2021-01-30 13:48:17 +0500

    Added notification helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  def notification
+    if flash[:notice]
+      content_tag(:div, class: 'alert-success alert-dismissible fade show', role: "alert") { 
+        notice
+      }
+    elsif flash[:alert]
+      content_tag(:div, class: 'alert-warning alert-dismissible fade show', role: "alert") { 
+        alert
+      }
+    end
+  end
+  
 end
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -2,10 +2,12 @@ module HomeHelper
   
   def header_nav_items
     if user_signed_in?
-      render 'shared/_header/signed_in'
+      render 'shared/header_helper/signed_in'
     else
-      render 'shared/_header/signed_out'
+      render 'shared/header_helper/signed_out'
     end
   end
+
+  
 
 end

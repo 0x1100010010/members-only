a110c0661d505052cadf31293353d676b6c9ff7d
bashforger <bash.forger@gmail.com>
2021-01-30 13:35:07 +0500

    Added header helper

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,11 @@
 module HomeHelper
+  
+  def header_nav_items
+    if user_signed_in?
+      render 'shared/_header/signed_in'
+    else
+      render 'shared/_header/signed_out'
+    end
+  end
+
 end
